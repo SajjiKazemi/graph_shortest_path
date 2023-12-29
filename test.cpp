31048e8e959ec7b38d94d924dad299d2014e25f2
sajji <sajjadkazemi.e@gmail.com>
2023-12-29 16:41:20 -0500

    Update test file

--- test.cpp
+++ test.cpp
@@ -4,11 +4,16 @@
  */
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
+#include "helpers.h"
 
-TEST_CASE("Successful Test Example") {
-    int a = 5;
-    CHECK(a == 5);
-}
-TEST_CASE("Failing Test Examples") {
-    CHECK(true == false);
+TEST_CASE("Test line_parser::get_command") {
+    std::string line = "V 15";
+    std::string command = line_parser::get_command(line);
+    CHECK(command == "V");
+    std::string line2 = "E {<1,2>,<2,3>,<3,4>}";
+    std::string command2 = line_parser::get_command(line2);
+    CHECK(command2 == "E");
+    std::string line3 = "s 1 2";
+    std::string command3 = line_parser::get_command(line3);
+    CHECK(command3 == "s");
 }
