c45c2efd34cdd3abcee413089fece540f3b3554e
sajji <sajjadkazemi.e@gmail.com>
2023-12-29 17:03:34 -0500

    Make some minor changes

--- main.cpp
+++ main.cpp
@@ -10,12 +10,12 @@ int main(int argc, char** argv) {
     // read from stdin until EOF
     MyGraph graph;
     while (!std::cin.eof()) {
-        
+
         std::string line;
         std::getline(std::cin, line);
 
         // get the command from the line
-        std::string command = line_parser::get_command(line);
+        std::string command = line_parser::get_command(line, graph);
         line_parser::analyze_command(command, line, graph);
     }
 }
