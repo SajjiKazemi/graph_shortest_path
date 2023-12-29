62da4ad1d0cbb3a4e2c90c4aaaef5cdf1de4a938
sajji <sajjadkazemi.e@gmail.com>
2023-12-29 17:01:02 -0500

    Update the main function.

--- main.cpp
+++ main.cpp
@@ -3,20 +3,19 @@
 #include <string>
 #include <vector>
 #include "helpers.h"
+#include "MyGraph.h"
 
 int main(int argc, char** argv) {
 
     // read from stdin until EOF
+    MyGraph graph;
     while (!std::cin.eof()) {
-        // print a promt
-        std::cout << "Enter numbers separated by comma: ";
-
-        // read a line of input until EOL and store in a string
+        
         std::string line;
         std::getline(std::cin, line);
 
         // get the command from the line
         std::string command = line_parser::get_command(line);
-        line_parser::analyze_command(command, line);
+        line_parser::analyze_command(command, line, graph);
     }
 }
